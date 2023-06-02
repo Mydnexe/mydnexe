3c5039d0cac059dc689e0b7ffd5b8921b925b885
[DZeynep <zaaydin1299@gmail.com>
2023-06-03 02:23:59 +0300

    ılk komıtım

---
+++ if.py
@@ -0,0 +1,16 @@
+# a 5
+a = 5
+#b=4
+y = "Hello, World!"
+if(True):
+    while(a>5):
+        print(a)
+        a=a-1
+        
+elif (a>20):
+    print(a>20)    
+    
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
